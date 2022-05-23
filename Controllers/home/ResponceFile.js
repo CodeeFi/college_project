c7949db1f28584177bd8249189ad1a9d8b8ff94f
CodeeFi <aadarshsingh121@gmail.com>
2022-05-23 23:50:53 +0530

    error Handling in reeponce file

--- Controllers/home/ResponceFile.js
+++ Controllers/home/ResponceFile.js
@@ -8,15 +8,18 @@ const wrapper = require("../../Middleware/wrapper");
 const ResponseFile = wrapper(async (req, res, next) => {
     const { url } = req.params;
 
+    if (!url) {
+        return next(customError("Bad Request", 400));
+    }
+
     const data = s3.getObject({ Bucket: process.env.AWS_BUCKET_NAME, Key: url }).createReadStream();
 
     data.on("error", (err) => {
         if (err)
             return next(customError("Invalid Url", 400));
     });
-    data.pipe(res);
+    return data.pipe(res);
 })
-
 
 module.exports = {
     ResponseFile
